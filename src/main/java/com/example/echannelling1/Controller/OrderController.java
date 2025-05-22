10213ac5995144c3ab4deb3f7b4ede5d113832c8
it24300019 <it24300019@my.sliit.lk>
2025-05-22 15:34:21 +0530

    Update OrderController.java

--- src/main/java/com/example/echannelling1/Controller/OrderController.java
+++ src/main/java/com/example/echannelling1/Controller/OrderController.java
@@ -9,8 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 
 @Controller
 @RequestMapping("/user")
