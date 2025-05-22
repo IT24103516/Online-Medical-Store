f5e19b5e169275885a5abe0adc0d8af097adc4f3
it24300019 <it24300019@my.sliit.lk>
2025-05-22 15:33:40 +0530

    updated imports of Order model

--- src/main/java/com/example/echannelling1/model/Order.java
+++ src/main/java/com/example/echannelling1/model/Order.java
@@ -2,7 +2,7 @@ package com.example.echannelling1.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class Order {
     private String orderId;
