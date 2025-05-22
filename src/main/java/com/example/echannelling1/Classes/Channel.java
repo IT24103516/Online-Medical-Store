49f9b23b0843b9a0cb92e22e2ec1c0be63bd9bab
Viha-N <nethmivihara0409@gmail.com>
2025-05-22 16:55:29 +0530

    Deleted unused construtors

--- src/main/java/com/example/echannelling1/Classes/Channel.java
+++ src/main/java/com/example/echannelling1/Classes/Channel.java
@@ -43,55 +43,11 @@ public class Channel {
     }
 
     public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-
-    public String getHospital() { return hospital; }
-    public void setHospital(String hospital) { this.hospital = hospital; }
 
     public String getDoctorName() { return doctorName; }
-    public void setDoctorName(String doctorName) { this.doctorName = doctorName; }
-
-    public String getSpecialization() { return specialization; }
-    public void setSpecialization(String specialization) { this.specialization = specialization; }
-
-    public String getDate() { return date; }
-    public void setDate(String date) { this.date = date; }
-
-    public String getTime() { return time; }
-    public void setTime(String time) { this.time = time; }
 
     public String getPatientName() { return patientName; }
-    public void setPatientName(String patientName) { this.patientName = patientName; }
 
-    public String getPatientAge() { return patientAge; }
-    public void setPatientAge(String patientAge) { this.patientAge = patientAge; }
-
-    public String getPatientGender() { return patientGender; }
-    public void setPatientGender(String patientGender) { this.patientGender = patientGender; }
-
-    public String getPatientNIC() { return patientNIC; }
-    public void setPatientNIC(String patientNIC) { this.patientNIC = patientNIC; }
-
-    public String getPatientPhone() { return patientPhone; }
-    public void setPatientPhone(String patientPhone) { this.patientPhone = patientPhone; }
-
-    public String getPatientEmail() { return patientEmail; }
-    public void setPatientEmail(String patientEmail) { this.patientEmail = patientEmail; }
-
-    public String getPatientAddress() { return patientAddress; }
-    public void setPatientAddress(String patientAddress) { this.patientAddress = patientAddress; }
-
-    public String getMedicalHistory() { return medicalHistory; }
-    public void setMedicalHistory(String medicalHistory) { this.medicalHistory = medicalHistory; }
-
-    public String getCurrentMedications() { return currentMedications; }
-    public void setCurrentMedications(String currentMedications) { this.currentMedications = currentMedications; }
-
-    public String getAllergies() { return allergies; }
-    public void setAllergies(String allergies) { this.allergies = allergies; }
-
-    public String getPaymentMethod() { return paymentMethod; }
-    public void setPaymentMethod(String paymentMethod) { this.paymentMethod = paymentMethod; }
 
     public String toFileString() {
         return String.join(",", id, hospital, doctorName, specialization, date, time,
