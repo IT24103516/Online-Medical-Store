51aacba403bd31ab01fd8ddf7263fd426afd6399
Havindu Sineth <it24300010@my.sliit.lk>
2025-05-22 13:55:58 +0530

    Queue and quicksort implemented

---
+++ src/main/java/com/example/echannelling1/Service/Queue.java
@@ -0,0 +1,39 @@
+package com.example.echannelling1.Service;
+
+import com.medicalstore.model.Order;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Queue {
+
+    private final LinkedList<Order> orderQueue;
+
+    public Queue() {
+        orderQueue = new LinkedList<>();
+    }
+
+    public void enqueue(Order order) {
+        orderQueue.addLast(order);
+    }
+
+    public Order dequeue() {
+        return orderQueue.pollFirst();
+    }
+
+    public boolean isEmpty() {
+        return orderQueue.isEmpty();
+    }
+
+    public List<Order> getAllOrders() {
+        return new LinkedList<>(orderQueue);
+    }
+
+    public int size() {
+        return orderQueue.size();
+    }
+
+    public void clear() {
+        orderQueue.clear();
+    }
+}
